f2b9e515def3752d2a515eb115e7279baa2dd0de
Sodare Dev <jtrejo.lid.mkt@gmail.com>
2026-07-23 17:17:43 -0600

    chore: cleanup debug endpoints, logging, and temp files

--- fetch_test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-async function go() {
-  const res = await fetch("http://localhost:3000/api/test-meta");
-  const json = await res.json();
-  fs.writeFileSync("meta.json", JSON.stringify(json, null, 2));
-  console.log("Done");
-}
-go();
